094a9e54749ffd99101c8fca6a53fb61c05f8a04
voel <voel@cba0054c-e3dd-4c1e-9f5d-83818dded0c2>
2008-08-19 17:17:05 +0000

    git-svn-id: file://localhost/home/voel/image/svn/yolan@50 cba0054c-e3dd-4c1e-9f5d-83818dded0c2

--- code/vm/MobyVM.java
+++ code/vm/MobyVM.java
@@ -1,2 +1,118 @@
+import java.util.Hashtable;
+import java.util.Stack;
+import java.util.Vector;
+
 class MobyVM {
+	class Code implements MobyFunction {
+		public Object[] closure;
+		public byte[] code;
+
+		public Code(Object o) {
+			int i;
+			Hashtable h = (Hashtable)o;
+
+			Vector v = (Vector)h.get("closure");
+			closure = new Object[v.size()];
+			for(i=0;i<v.size();i++) {
+				closure[i] = v.elementAt(i);
+			}
+
+			v = (Vector)h.get("code");
+			code = new byte[v.size()];
+			for(i=0;i<v.size();i++) {
+				code[i] = ((Integer)v.elementAt(i)).byteValue();
+			}
+		}
+
+		public Object apply(MobyVM vm) {
+			return vm.eval((Code) this);
+		}
+	}
+
+	class Builtin implements MobyFunction {
+		private int id;
+
+		public Builtin(int id) {
+			this.id = id;
+		}
+
+		public Object apply(MobyVM vm) {
+			int i;
+			switch(id) {
+		// print
+		case 0: 
+			for(i=0;i<vm.argc;i++) {
+				System.out.println(vm.getArg(i));
+			}
+		break;
+			}
+
+			return null;
+		}
+	}
+
+	Hashtable globals;
+	Stack stack;
+	int argc;
+
+	Object getArg(int n) {
+		return stack.elementAt(stack.size() - argc + n);
+	}
+
+	int getArgI(int n) {
+		return toInt(getArg(n));
+	}
+
+	String getArgS(int n) {
+		return (String)getArg(n);
+	}
+
+	public void log(String s) {
+		System.out.println(s);
+	}
+	public int toInt(Object o) {
+		return ((Integer)o).intValue();
+	}
+
+	
+	public MobyVM() {
+		stack = new Stack();
+		globals = new Hashtable();
+
+		globals.put("print", new Builtin(0));
+	}
+
+	public Object eval(String s) {
+		return this;
+	}
+
+	public Object eval(Object o) {
+		return eval(new Code(o));
+	}
+
+	public Object eval(Code c) {
+		byte[] code = c.code;
+		Object[] closure = c.closure;
+		int pc, codesize, i;
+		Object o;
+		codesize = code.length;
+
+		for(pc=0;pc < codesize;pc++) {
+			switch(code[pc]) {
+	// push from closure (closure id)
+	case 0: 
+		pc++;
+		stack.push(closure[code[pc]]);
+		break;
+	// call global (number of arguments, closure id of name)
+	case 1:
+		pc++; 
+		argc = code[pc];
+		pc++;
+		((MobyFunction)globals.get(closure[code[pc]])).apply(this);
+		break;
+			}
+		}
+		return null;
+	}
 }
--- code/vm/Test.java
+++ code/vm/Test.java
@@ -5,7 +5,11 @@ import java.io.InputStream;
 class Test {
 	public static void main(String[] args) throws java.io.IOException {
 		InputStream is = new FileInputStream(new File(args[0]));
-		JSON.print(JSON.parse(is), System.out);
+		Object o = JSON.parse(is);
+		JSON.print(o, System.out);
 		System.out.println("");
+
+		MobyVM vm = new MobyVM();
+		vm.eval(o);
 	}
 }
