021f57427bb62c493fe8e25936fa74ec4d55c558
voel <voel@cba0054c-e3dd-4c1e-9f5d-83818dded0c2>
2008-09-04 19:46:07 +0000

    git-svn-id: file://localhost/home/voel/image/svn/yolan@115 cba0054c-e3dd-4c1e-9f5d-83818dded0c2

--- code/compiler/test.sh
+++ code/compiler/test.sh
@@ -5,4 +5,7 @@ diff parser.js /tmp/t.js
 echo "Recompiling JavaScript Backend..."
 js run.js < backend_js.js > /tmp/t.js
 diff backend_js.js /tmp/t.js
+echo "Recompiling Yolan Parser..."
+js run.js < yolan.js > /tmp/t.js
+diff yolan.js /tmp/t.js
 echo "Done."
