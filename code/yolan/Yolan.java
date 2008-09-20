c1d004abf0ef3b35d1b86bd8ef7d85caf91997bd
voel <voel@cba0054c-e3dd-4c1e-9f5d-83818dded0c2>
2008-09-20 19:07:19 +0000

    git-svn-id: file://localhost/home/voel/image/svn/yolan@149 cba0054c-e3dd-4c1e-9f5d-83818dded0c2

--- code/yolan/Yolan.java
+++ code/yolan/Yolan.java
@@ -1,13 +1,14 @@
+
+import java.util.Enumeration;
 import java.util.Stack;
 import java.util.Hashtable;
-import java.util.Enumeration;
 import java.io.InputStream;
 
 final class Yolan extends Stack {
 	public Hashtable globals;
 	private Stack functions;
 	private Stack literals;
-	private Hashtable literalid;
+	private Hashtable literalid; 
 	private int prevpos;
 	private static final String builtins[] = { "print", "+", "<", "if-else", "drop", "defun", "[", "]", "repeat-until", "dup", "dup2", "a.pop", "a.push", "a.is-empty", "dup3", "print-stack", "dup4", "swap", "rot", "a.copy", "a.reverse", "call2f", "a.iterator", "more-elements", "next-element", "not", "while", "dup4", "put", "put2", "put3", "string-join"};
 	private static final int builtincount = builtins.length;
@@ -42,12 +43,7 @@ final class Yolan extends Stack {
 		int c = is.read();
 
 		// skip whitespaces 
-		while(c <= '!') {
-			if(c == '!') {
-				while(c != '\n' && c != -1) {
-					c = is.read();
-				}
-			}
+		while(c <= ' ') {
 			c = is.read();
 			if(c == -1) {
 				return -1;
@@ -56,7 +52,7 @@ final class Yolan extends Stack {
 
 		// parse string
 		if(c == '\"') {
-			while((((c = is.read()) == '\"' && (c = is.read()) <= '!' )?-1:c) != -1) {
+			while((((c = is.read()) == '\"' && (c = is.read()) <= ' ' )?-1:c) != -1) {
 				sb.append((char)c);
 			}
 			return getid(sb.toString());
@@ -75,7 +71,7 @@ final class Yolan extends Stack {
 		// parse integer or word
 		do {
 			sb.append((char)c);
-		} while((c = is.read()) > '!');
+		} while((c = is.read()) > ' ');
 
 		String s = sb.toString();
 
@@ -97,11 +93,11 @@ final class Yolan extends Stack {
 	}
 
 	public Yolan() {
+		prevpos = 0;
 		globals = new Hashtable();
 		literalid = new Hashtable();
 		literals = new Stack();
 		functions = new Stack();
-		prevpos = 0;
 		for(char i = 0; i < builtincount; i++) {
 			char fn[] = {i};
 			globals.put(builtins[i], new Character(i));
@@ -122,23 +118,34 @@ final class Yolan extends Stack {
 		for(pc = 0; pc < code.length; pc++) {
 			switch(code[pc]) {
 /* BEGIN SWITCH*/
-	/* print */ case 0: 
-	{
+
+    
+/* print*/ case  0: 
+    { 
 		Util.print(pop());
-	}
-	/* + */ break; case 1: 
-	{
+	
+    } 
+    break; 
+    
+/* +*/ case  1: 
+    { 
 		push(new Integer(
 			((Integer)pop()).intValue() +
 			((Integer)pop()).intValue()));
-	}
-	/* < */ break; case 2: 
-	{
+	
+    } 
+    break; 
+    
+/* <*/ case  2: 
+    { 
 		push( (((Integer)pop()).intValue() >
 			((Integer)pop()).intValue())?t:null);
-	}
-	/* if-else */ break; case 3: 
-	{
+	
+    } 
+    break; 
+    
+/* if-else*/ case  3: 
+    { 
 		if(pop() == null) {
 			pop();
 			eval((char[])pop());
@@ -147,25 +154,37 @@ final class Yolan extends Stack {
 			pop();
 			eval(expr);
 		}
-	}
-	/* drop */ break; case 4: 
-	{
+	
+    } 
+    break; 
+    
+/* drop*/ case  4: 
+    { 
 		pop();
-	}
-	/* defun */ break; case 5: 
-	{
+	
+    } 
+    break; 
+    
+/* defun*/ case  5: 
+    { 
 		String name = (String) pop();
 		Object codetext = pop();
 		globals.put(name, new Character((char)(functions.size() + builtincount)));
 		functions.push(codetext);
-	}
-	/* [ */ break; case 6: 
-	{
+	
+    } 
+    break; 
+    
+/* [*/ case  6: 
+    { 
 		push(new Integer(prevpos));
 		prevpos = size();
-	}
-	/* ] */ break; case 7: 
-	{
+	
+    } 
+    break; 
+    
+/* ]*/ case  7: 
+    { 
 		Stack s = new Stack();
 		int count = size() - prevpos;
 		s.setSize(size() - prevpos);
@@ -175,57 +194,90 @@ final class Yolan extends Stack {
 		setSize(prevpos);
 		prevpos = ((Integer)pop()).intValue();
 		push(s);
-	}
-	/* repeat-until */ break; case 8: 
-	{
+	
+    } 
+    break; 
+    
+/* repeat-until*/ case  8: 
+    { 
 		char [] expr = (char []) pop();
 		do {
 			eval(expr);
 		} while(pop() == null);
-	}
-	/* dup */ break; case 9: 
-	{
+	
+    } 
+    break; 
+    
+/* dup*/ case  9: 
+    { 
 		push(peek());
-	}
-	/* dup2 */ break; case 10: 
-	{
+	
+    } 
+    break; 
+    
+/* dup2*/ case  10: 
+    { 
 		push(elementAt(size() - 2));
-	}
-	/* a.pop */ break; case 11: 
-	{
+	
+    } 
+    break; 
+    
+/* a.pop*/ case  11: 
+    { 
 		push(((Stack)pop()).pop());
-	}
-	/* a.push */ break; case 12: 
-	{
+	
+    } 
+    break; 
+    
+/* a.push*/ case  12: 
+    { 
 		Object o = pop();
 		((Stack)peek()).push(o);
-	}
-	/* a.is-empty */ break; case 13: 
-	{
+	
+    } 
+    break; 
+    
+/* a.is-empty*/ case  13: 
+    { 
 		push((((Stack)pop()).size() == 0)?t:null);
-	}
-	/* dup3 */ break; case 14: 
-	{
+	
+    } 
+    break; 
+    
+/* dup3*/ case  14: 
+    { 
 		push(elementAt(size() - 3));
-	}
-	/* print-stack */ break; case 15: 
-	{
+	
+    } 
+    break; 
+    
+/* print-stack*/ case  15: 
+    { 
 		Util.print(this.toString());
-	}
-	/* dup4 */ break; case 16: 
-	{
+	
+    } 
+    break; 
+    
+/* dup4*/ case  16: 
+    { 
 		push(elementAt(size() - 4));
-	}
-	/* swap */ break; case 17: 
-	{
+	
+    } 
+    break; 
+    
+/* swap*/ case  17: 
+    { 
 		Object a, b;
 		a = pop();
 		b = pop();
 		push(a);
 		push(b);
-	}
-	/* rot */ break; case 18: 
-	{
+	
+    } 
+    break; 
+    
+/* rot*/ case  18: 
+    { 
 		Object a, b, c;
 		a = pop();
 		b = pop();
@@ -233,9 +285,12 @@ final class Yolan extends Stack {
 		push(b);
 		push(a);
 		push(c);
-	}
-	/* a.copy */ break; case 19: 
-	{
+	
+    } 
+    break; 
+    
+/* a.copy*/ case  19: 
+    { 
 		Stack src, dst;
 		src = (Stack)pop();
 		dst = new Stack();
@@ -244,9 +299,12 @@ final class Yolan extends Stack {
 			dst.setElementAt(src.elementAt(i), i);
 		}
 		push(dst);
-	}
-	/* a.reverse */ break; case 20: 
-	{
+	
+    } 
+    break; 
+    
+/* a.reverse*/ case  20: 
+    { 
 		Object t;
 		Stack s = (Stack)peek();
 		int a = 0, b = s.size() - 1;
@@ -256,29 +314,47 @@ final class Yolan extends Stack {
 			s.setElementAt(t, b);
 			a++; b--;
 		}
-	}
-	/* call2f */ break; case 21: 
-	{
+	
+    } 
+    break; 
+    
+/* call2f*/ case  21: 
+    { 
 		eval((char []) pop());
-	}
-	/* a.iterator */ break; case 22: 
-	{
+	
+    } 
+    break; 
+    
+/* a.iterator*/ case  22: 
+    { 
 		push(((Stack)pop()).elements());
-	}
-	/* more-elements */ break; case 23: 
-	{
+	
+    } 
+    break; 
+    
+/* more-elements*/ case  23: 
+    { 
 		push(((Enumeration)pop()).hasMoreElements()?t:null);
-	}
-	/* next-element */ break; case 24: 
-	{
+	
+    } 
+    break; 
+    
+/* next-element*/ case  24: 
+    { 
 		push(((Enumeration)pop()).nextElement());
-	}
-	/* not */ break; case 25: 
-	{
+	
+    } 
+    break; 
+    
+/* not*/ case  25: 
+    { 
 		push((pop() == null)?t:null);
-	}
-	/* while */ break; case 26: 
-	{
+	
+    } 
+    break; 
+    
+/* while*/ case  26: 
+    { 
 		char [] cond, expr;
 		expr = (char [])pop();
 		cond = (char [])pop();
@@ -287,33 +363,50 @@ final class Yolan extends Stack {
 			eval(expr);
 			eval(cond);
 		}
-	}
-	/* dup4 */ break; case 27: 
-	{
+	
+    } 
+    break; 
+    
+/* dup4*/ case  27: 
+    { 
 		push(elementAt(size() - 4));
-	}
-	/* put */ break; case 28: 
-	{
+	
+    } 
+    break; 
+    
+/* put*/ case  28: 
+    { 
 		setElementAt(pop(), size() - 1);
-	}
-	/* put2 */ break; case 29: 
-	{
+	
+    } 
+    break; 
+    
+/* put2*/ case  29: 
+    { 
 		setElementAt(pop(), size() - 2);
-	}
-	/* put3 */ break; case 30: 
-	{
+	
+    } 
+    break; 
+    
+/* put3*/ case  30: 
+    { 
 		setElementAt(pop(), size() - 3);
-	}
-	/* string-join */ break; case 31: 
-	{
+	
+    } 
+    break; 
+    
+/* string-join*/ case  31: 
+    { 
 		StringBuffer sb = new StringBuffer();
 		Stack s = (Stack)pop();
 		for(int i = 0; i<s.size();i++) {
 			sb.append(s.elementAt(i).toString());
 		}
 		push(sb.toString());
-	}
-	/* Literals or userdefined */ break; default:
+	
+    } 
+    break; 
+	/* Literals or userdefined */ default:
 		if(code[pc] >= firstliteral) {
 			push(literals.get(code[pc] - firstliteral));
 		} else {
@@ -324,3 +417,4 @@ final class Yolan extends Stack {
 		}
 	}
 }
+
