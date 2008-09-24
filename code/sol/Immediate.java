4e61c1e1478282cb0863076dabc2fe10c2a715d1
voel <voel@cba0054c-e3dd-4c1e-9f5d-83818dded0c2>
2008-09-24 05:13:38 +0000

    git-svn-id: file://localhost/home/voel/image/svn/yolan@164 cba0054c-e3dd-4c1e-9f5d-83818dded0c2

---
+++ code/sol/Immediate.java
@@ -0,0 +1,40 @@
+import java.util.Stack;
+
+public final class Immediate extends Function {
+	private Object val;
+
+	public Immediate(Object val) {
+		this.val = val;
+	}
+
+	public void apply(Stack s) {
+		s.push(val);
+	}
+
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		if(val instanceof String) {
+			String s = (String) val;
+			sb.append("\"");
+			for(int i = 0; i < s.length(); i++) {
+				char c = s.charAt(i);
+				if(c == '\\' || c == '"') {
+					sb.append('\\');
+				}
+				sb.append(c);
+			}
+			sb.append("\"");
+		} else if(val instanceof Function[]) {
+			Function code[] = ((Function[]) val);
+			sb.append("( ");
+			for(int i = 0; i< code.length; i++) {
+				sb.append(code[i].toString());
+				sb.append(" ");
+			}
+			sb.append(")");
+		} else {
+			return val.toString();
+		}
+		return sb.toString();
+	}
+}
