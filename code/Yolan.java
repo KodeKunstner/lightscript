6aa08925d08c54d33b4d17f6409d41811ce54e99
voel <voel@cba0054c-e3dd-4c1e-9f5d-83818dded0c2>
2008-10-06 16:30:43 +0000

    git-svn-id: file://localhost/home/voel/image/svn/yolan@197 cba0054c-e3dd-4c1e-9f5d-83818dded0c2

--- code/Yolan.java
+++ code/Yolan.java
@@ -10,8 +10,15 @@ final class Yolan {
 		closure = null;
 	}
 	private static Hashtable globals = new Hashtable();
+
+	// Initialisation code, 
+	// with compact representation of function names
 	static {
-		String str = "println + - * / < num if while set! do ";
+		// space terminated names of builtin functions
+		// in the same order as the implementation in apply
+		String str = "println + - * / < num if while set! do str ";
+
+		// parse the names, creat new yolan function, and add it to the globals
 		int i, j, k;
 		k = i = j = 0;
 		while(i < str.length()) {
@@ -24,20 +31,28 @@ final class Yolan {
 		}
 	}
 
+	// Builtin functions and macros
 	private Object apply(Object X[]) {
 		switch(fn) {
-			case 0: { 	
-					Object arg = e(X[1]); 
+			// println
+			case 0: { 	Object arg = e(X[1]); 
 					System.out.println(arg); 
-					return arg;
-			}
+					return arg; }
+			// +
 			case 1: return num(ival(e(X[1])) + ival(e(X[2])));
+			// -
 			case 2: return num(ival(e(X[1])) + ival(e(X[2])));
+			// *
 			case 3: return num(ival(e(X[1])) + ival(e(X[2])));
+			// /
 			case 4: return num(ival(e(X[1])) + ival(e(X[2])));
+			// <
 			case 5: return ival(e(X[1])) < ival(e(X[2])) ? X:null;
+			// num
 			case 6: return Integer.valueOf((String)X[1]);
+			// if
 			case 7: return (e(X[1]) != null)?e(X[2]):e(X[3]);
+			// while
 			case 8: {	
 					Object result = null;
 					while(e(X[1]) != null) {
@@ -45,30 +60,37 @@ final class Yolan {
 					}
 					return result; 
 			}
+			// set!
 			case 9: {	
 					Object o = e(X[2]);
 					globals.put(X[1], o);
 					return o; 
 			}
+			// do
 			case 10: {	Object result = null;
 					for(int i = 1; i < X.length; i++) {
 						result = e(X[i]);
 					}
 					return result;
 			}
+			// str 
+			case 11: return X[1];
 		}
 		return null;
 	}
 	
 
+	// shorthand conversion from object to integer
 	private static int ival(Object o) {
 		return ((Integer)o).intValue();
 	}
 
+	// shorthand conversion from integer to object
 	private static Object num(int i) {
 		return new Integer(i);
 	}
 
+	// evaluate list or lookup symbol
 	private static Object e(Object o) {
 		if(o instanceof Object[]) {
 			Object X[] = ((Object [])o);
@@ -82,6 +104,7 @@ final class Yolan {
 		}
 	}
 
+	// parse a list of expressions
 	private static Object[] parse(InputStream is) throws Exception {
 		int c;
 		Stack s = new Stack();
@@ -115,6 +138,7 @@ final class Yolan {
 		return result;
 	}
 
+	// parse and evaluate code from input stream
 	public static Object eval(InputStream is) throws Exception {
 		Object exprs[] = parse(is);
 		Object result = null;
@@ -124,6 +148,7 @@ final class Yolan {
 		return result;
 	}
 
+	// main/test-function
 	public static void main(String X[]) throws Exception {
 		eval(new FileInputStream(new File(X[0])));
 	}
