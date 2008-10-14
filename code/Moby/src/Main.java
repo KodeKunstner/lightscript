0d6525d44122740e374d387755512fa393624ad7
voel <voel@cba0054c-e3dd-4c1e-9f5d-83818dded0c2>
2008-10-14 06:14:46 +0000

    git-svn-id: file://localhost/home/voel/image/svn/yolan@203 cba0054c-e3dd-4c1e-9f5d-83818dded0c2

---
+++ code/Moby/src/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.microedition.midlet.*;
+import javax.microedition.lcdui.*;
+
+/**
+ * @author voel
+ */
+public class Main extends MIDlet {
+    public void startApp() {
+    }
+
+    public void pauseApp() {
+    }
+
+    public void destroyApp(boolean unconditional) {
+    }
+}
