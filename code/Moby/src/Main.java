f9d5329d9da70483935ec4a0ceaa6590080ab537
voel <voel@cba0054c-e3dd-4c1e-9f5d-83818dded0c2>
2008-12-31 14:48:24 +0000

    git-svn-id: file://localhost/home/voel/image/svn/yolan@230 cba0054c-e3dd-4c1e-9f5d-83818dded0c2

--- code/Moby/src/Main.java
+++ code/Moby/src/Main.java
@@ -1,16 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import javax.microedition.midlet.*;
 import javax.microedition.lcdui.*;
 
-/**
- * @author voel
- */
 public class Main extends MIDlet {
     static Form form;
     public void startApp() {
