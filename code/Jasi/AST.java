d9df3d6f5b33c48269137a97a188a38bb4d9a051
voel <voel@cba0054c-e3dd-4c1e-9f5d-83818dded0c2>
2009-02-06 17:35:17 +0000

    git-svn-id: file://localhost/home/voel/image/svn/yolan@275 cba0054c-e3dd-4c1e-9f5d-83818dded0c2

--- code/Jasi/AST.java
+++ code/Jasi/AST.java
@@ -9,42 +9,149 @@ import java.util.Stack;
  * Abstract Syntax Tree
  */
 public class AST {
-	private static final byte AST_IDENTIFIER = 0;
-	private static final byte AST_LITERAL = 1;
-	private static final byte AST_LIST = 2;
-	private byte type;
+
+	private static final char OP_LITERAL = 'l';
+	private static final char OP_ADD = 'a';
+	private static final char OP_SUB = 'b';
+	private static final char OP_RET = 0xFF;
+
+	private static final int MASK_OP = 0xFF;
+	private static final int MASK_BUILTIN_BINOP = 0x100;
+	private static final int MASK_BUILTIN_UNOP = 0x200;
+
+	private static final int AST_IDENTIFIER = 0;
+	private static final int AST_LITERAL = AST_IDENTIFIER + 1;
+	private static final int AST_LIST = AST_LITERAL + 1;
+	private static final int AST_ADD = MASK_BUILTIN_BINOP | OP_ADD;
+	private static final int AST_SUB = MASK_BUILTIN_BINOP | OP_SUB;
+
+	private int type;
 	private Object val;
 	private AST[] tree;
+	private static final AST[] emptytree = new AST[0];
+
+
+	private static String[] builtin_names = {"+", "-"};
+	private static int[] builtin_types = {AST_ADD, AST_SUB};
+
+	private static void pass_ast_type(AST ast) {
+		for(int i = 0; i < ast.tree.length; i++) {
+			pass_ast_type(ast.tree[i]);
+		}
+		if(ast.tree.length > 0) {
+			for(int i = 0; i < builtin_names.length; i++) {
+				if(ast.tree[0].val.equals(builtin_names[i])) {
+					ast.type = builtin_types[i];
+					return;
+				}
+			}
+		}
+	}
+
+	public static class CompiledExpr {
+		public Object literals[];
+		public byte code[];
+		public CompiledExpr(StringBuffer sb, Stack stack) {
+			code = new byte[sb.length()];
+			for(int i = 0; i< code.length; i++) {
+				code[i] = (byte)sb.charAt(i);
+			}
+			literals = new Object[stack.size()];
+			stack.copyInto(literals);
+		}
+	}
+
+	private static void pass_emit(StringBuffer code_acc, Stack literals, AST ast) {
+		if((ast.type & MASK_BUILTIN_BINOP) != 0) {
+			pass_emit(code_acc, literals, ast.tree[1]);
+			pass_emit(code_acc, literals, ast.tree[2]);
+			code_acc.append((char)(ast.type & MASK_OP));
+
+		} else if((ast.type & AST_LITERAL) != 0) {
+			code_acc.append((char)OP_LITERAL);
+
+			int pos = literals.indexOf(ast.val);
+			if(pos < 0) {
+				pos = literals.size();
+				literals.push(ast.val);
+			}
+
+			if(pos > 255) {
+				throw new Error("Too many literals in expression");
+			}
+			code_acc.append((char)pos);
+		}
+	}
+
+	public static CompiledExpr compile(AST ast) {
+		pass_ast_type(ast);
+
+		StringBuffer code_acc = new StringBuffer();
+		Stack literals = new Stack();
+		pass_emit(code_acc, literals, ast);
+		System.out.println("code:" + code_acc.toString());
+
+		return new CompiledExpr(code_acc, literals);
+	}
+
+	public static Object execute(CompiledExpr coexp) {
+		Stack stack = new Stack();
+		byte code[] = coexp.code;
+		Object literals[] = coexp.literals;
+		for(int pc=0; pc < code.length; pc++) {
+			System.out.println("pc:" + pc + " op:" + code[pc]);
+			switch(code[pc]) {
+				case OP_LITERAL: 
+					stack.push(literals[code[++pc]]);
+					break;
+				case OP_ADD:
+					stack.push(new Integer(
+						((Integer)stack.pop()).intValue() +
+						((Integer)stack.pop()).intValue()));
+					break;
+				case OP_SUB:
+					stack.push(new Integer(
+						- ((Integer)stack.pop()).intValue() +
+						((Integer)stack.pop()).intValue()));
+					break;
+			}
+		}
+		return stack.pop();
+	}
 
 	private AST(AST[] t) {
 		type = AST_LIST;
+		val = null;
 		tree = t;
 	}
 
 	private AST(String s) {
 		type = AST_IDENTIFIER;
 		val = s;
+		tree = emptytree;
 	}
 
 	private AST(Object o) {
 		type = AST_LITERAL;
 		val = o;
+		tree = emptytree;
 	}
 
 	public String toString() {
-		StringBuffer sb = new StringBuffer();
 		if(type == AST_IDENTIFIER) {
-			sb.append(" id_").append(val);
+			return " id(" + val + ")";
 		} else if(type == AST_LITERAL) {
-			sb.append(" ").append(val);
+			return " literal(" + val + ")";
 		} else if(type == AST_LIST) {
+			StringBuffer sb = new StringBuffer();
 			sb.append("[");
 			for(int i = 0 ; i < tree.length; i++) {
 				sb.append(tree[i].toString());
 			}
 			sb.append(" ]");
+			return sb.toString();
 		} 
-		return sb.toString();
+		throw new java.lang.Error("Unhandled case");
 	}
 
     /////////////////////////////////////
@@ -74,7 +181,7 @@ public class AST {
                 int pos = stack.search(null);
                 // ] with no [ begun
                 if (pos == -1) {
-                    result = new AST[0];
+                    result = emptytree;
                 } else {
                     // stack search includes the null, which we want to skip
                     pos--;
--- code/Jasi/Main.java
+++ code/Jasi/Main.java
@@ -9,6 +9,6 @@ public class Main {
      */
     public static void main(String[] args) throws Exception {
 	InputStream is = new FileInputStream(new File(args[0]));
-	System.out.println(AST.readExpression(is).toString());
+	System.out.println(AST.execute(AST.compile(AST.readExpression(is))));
     }
 }
