82b0690e8f0dff0cc2e35436940dc043e98427b4
voel <voel@cba0054c-e3dd-4c1e-9f5d-83818dded0c2>
2009-02-08 04:41:48 +0000

    git-svn-id: file://localhost/home/voel/image/svn/yolan@276 cba0054c-e3dd-4c1e-9f5d-83818dded0c2

--- code/Jasi/AST.java
+++ code/Jasi/AST.java
@@ -9,39 +9,66 @@ import java.util.Stack;
  * Abstract Syntax Tree
  */
 public class AST {
-
-	private static final char OP_LITERAL = 'l';
-	private static final char OP_ADD = 'a';
-	private static final char OP_SUB = 'b';
-	private static final char OP_RET = 0xFF;
+	private static final char OP_LITERAL = 0;
+	private static final char OP_NOT = 1;
+	private static final char OP_ADD = 2;
+	private static final char OP_SUB = 3;
+	private static final char OP_MUL = 4;
+	private static final char OP_DIV = 5;
+	private static final char OP_REM = 6;
+	private static final char OP_IS_INT = 7;
+	private static final char OP_IS_STR = 8;
+	private static final char OP_IS_LIST = 9;
+	private static final char OP_IS_DICT = 10;
+	private static final char OP_IS_ITER = 11;
+	private static final char OP_EQUAL = 12;
+	private static final char OP_IS_EMPTY = 13;
+	private static final char OP_PUT = 14;
+	private static final char OP_GET = 15;
+	private static final char OP_RAND = 16;
+	private static final char OP_SIZE = 17;
+	private static final char OP_LESS = 18;
+	private static final char OP_LESSEQUAL = 19;
+	private static final char OP_SUBSTR = 20;
+	private static final char OP_RESIZE = 21;
+	private static final char OP_PUSH = 22;
+	private static final char OP_POP = 23;
+	private static final char OP_KEYS = 24;
+	private static final char OP_VALUES = 25;
+	private static final char OP_NEXT = 26;
+	private static final char OP_LOG = 27;
+	private static final char OP_ASSERT= 28;
 
 	private static final int MASK_OP = 0xFF;
-	private static final int MASK_BUILTIN_BINOP = 0x100;
-	private static final int MASK_BUILTIN_UNOP = 0x200;
-
 	private static final int AST_IDENTIFIER = 0;
 	private static final int AST_LITERAL = AST_IDENTIFIER + 1;
 	private static final int AST_LIST = AST_LITERAL + 1;
-	private static final int AST_ADD = MASK_BUILTIN_BINOP | OP_ADD;
-	private static final int AST_SUB = MASK_BUILTIN_BINOP | OP_SUB;
+	private static final int AST_FUNCTION = 0x100;
 
 	private int type;
+	private int arity;
 	private Object val;
 	private AST[] tree;
 	private static final AST[] emptytree = new AST[0];
 
+	public static final Boolean TRUE = new Boolean(true);
 
-	private static String[] builtin_names = {"+", "-"};
-	private static int[] builtin_types = {AST_ADD, AST_SUB};
+	private static String[] fn_names = {"not", "+", "-", "*", "/", "%", "is-integer", "is-string", "is-list", "is-dictionary", "is-iterator", "equals", "is-empty", "put", "get", "random", "size", "<", "<=", "substring", "resize", "push", "pop", "keys", "values", "get-next", "log", "assert"};
+	private static int[] fn_arity = {1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 2, 1, 3, 2, 1, 1, 2, 2, 3, 2, 2, 1, 1, 1, 1, 1, 2};
+	private static char[] fn_types = {OP_NOT, OP_ADD, OP_SUB, OP_MUL, OP_DIV, OP_REM, OP_IS_INT, OP_IS_STR, OP_IS_LIST, OP_IS_DICT, OP_IS_ITER, OP_EQUAL, OP_IS_EMPTY, OP_PUT, OP_GET, OP_RAND, OP_SIZE, OP_LESS, OP_LESSEQUAL, OP_SUBSTR, OP_RESIZE, OP_PUSH, OP_POP, OP_KEYS, OP_VALUES, OP_NEXT, OP_LOG, OP_ASSERT};
 
 	private static void pass_ast_type(AST ast) {
 		for(int i = 0; i < ast.tree.length; i++) {
 			pass_ast_type(ast.tree[i]);
 		}
 		if(ast.tree.length > 0) {
-			for(int i = 0; i < builtin_names.length; i++) {
-				if(ast.tree[0].val.equals(builtin_names[i])) {
-					ast.type = builtin_types[i];
+			for(int i = 0; i < fn_names.length; i++) {
+				if(ast.tree[0].val.equals(fn_names[i])) {
+					if(ast.tree.length != fn_arity[i] + 1) {
+						throw new Error("Wrong number of arguments: " + ast.toString());
+					}
+					ast.type = AST_FUNCTION | fn_types[i];
+					ast.arity = fn_arity[i];
 					return;
 				}
 			}
@@ -62,12 +89,13 @@ public class AST {
 	}
 
 	private static void pass_emit(StringBuffer code_acc, Stack literals, AST ast) {
-		if((ast.type & MASK_BUILTIN_BINOP) != 0) {
-			pass_emit(code_acc, literals, ast.tree[1]);
-			pass_emit(code_acc, literals, ast.tree[2]);
+		if((ast.type & AST_FUNCTION) != 0) {
+			for(int i = 1; i <= ast.arity; i++) {
+				pass_emit(code_acc, literals, ast.tree[i]);
+			}
 			code_acc.append((char)(ast.type & MASK_OP));
 
-		} else if((ast.type & AST_LITERAL) != 0) {
+		} else if(ast.type == AST_LITERAL) {
 			code_acc.append((char)OP_LITERAL);
 
 			int pos = literals.indexOf(ast.val);
@@ -77,7 +105,7 @@ public class AST {
 			}
 
 			if(pos > 255) {
-				throw new Error("Too many literals in expression");
+				throw new Error("Too many literals in expression: " + ast.toString());
 			}
 			code_acc.append((char)pos);
 		}
@@ -101,19 +129,59 @@ public class AST {
 		for(int pc=0; pc < code.length; pc++) {
 			System.out.println("pc:" + pc + " op:" + code[pc]);
 			switch(code[pc]) {
-				case OP_LITERAL: 
+				case OP_LITERAL:  {
 					stack.push(literals[code[++pc]]);
-					break;
-				case OP_ADD:
+	break; } case OP_NOT : {
+	break; } case OP_ADD : {
 					stack.push(new Integer(
 						((Integer)stack.pop()).intValue() +
 						((Integer)stack.pop()).intValue()));
-					break;
-				case OP_SUB:
+	break; } case OP_SUB : {
 					stack.push(new Integer(
 						- ((Integer)stack.pop()).intValue() +
 						((Integer)stack.pop()).intValue()));
-					break;
+	break; } case OP_MUL : {
+					stack.push(new Integer(
+						((Integer)stack.pop()).intValue() *
+						((Integer)stack.pop()).intValue()));
+	break; } case OP_DIV : {
+					stack.push(new Integer(
+						((Integer)stack.pop()).intValue() /
+						((Integer)stack.pop()).intValue()));
+	break; } case OP_REM : {
+					stack.push(new Integer(
+						((Integer)stack.pop()).intValue() %
+						((Integer)stack.pop()).intValue()));
+	break; } case OP_IS_INT : {
+		stack.push(stack.pop() instanceof Integer ? TRUE : null);
+	break; } case OP_IS_STR : {
+		stack.push(stack.pop() instanceof String ? TRUE : null);
+	break; } case OP_IS_LIST : {
+		stack.push(stack.pop() instanceof Stack ? TRUE : null);
+	break; } case OP_IS_DICT : {
+		stack.push(stack.pop() instanceof Hashtable ? TRUE : null);
+	break; } case OP_IS_ITER : {
+		stack.push(stack.pop() instanceof Enumeration ? TRUE : null);
+	break; } case OP_EQUAL : {
+		stack.push(stack.pop().equals(stack.pop()) ? TRUE : null);
+	break; } case OP_IS_EMPTY : {
+	break; } case OP_PUT : {
+	break; } case OP_GET : {
+	break; } case OP_RAND : {
+	break; } case OP_SIZE : {
+	break; } case OP_LESS : {
+	break; } case OP_LESSEQUAL : {
+	break; } case OP_SUBSTR : {
+	break; } case OP_RESIZE : {
+	break; } case OP_PUSH : {
+	break; } case OP_POP : {
+	break; } case OP_KEYS : {
+	break; } case OP_VALUES : {
+	break; } case OP_NEXT : {
+	break; } case OP_LOG : {
+		System.out.println(stack.peek());
+	break; } case OP_ASSERT: {
+	}
 			}
 		}
 		return stack.pop();
@@ -150,8 +218,15 @@ public class AST {
 			}
 			sb.append(" ]");
 			return sb.toString();
+		} else {
+			StringBuffer sb = new StringBuffer();
+			sb.append("{" + type);
+			for(int i = 0 ; i < tree.length; i++) {
+				sb.append(tree[i].toString());
+			}
+			sb.append(" }");
+			return sb.toString();
 		} 
-		throw new java.lang.Error("Unhandled case");
 	}
 
     /////////////////////////////////////
