24ef43901f34ecb8c39d5653dd61318aa9cd7306
voel <voel@cba0054c-e3dd-4c1e-9f5d-83818dded0c2>
2009-02-08 19:31:55 +0000

    git-svn-id: file://localhost/home/voel/image/svn/yolan@295 cba0054c-e3dd-4c1e-9f5d-83818dded0c2

---
+++ code/ys/Main.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Hashtable;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+	InputStream is = new FileInputStream(new File(args[0]));
+	System.out.println(Ys.stringify(Ys.readExpression(is)));
+    }
+}
---
+++ code/ys/Ys.java
@@ -0,0 +1,222 @@
+import java.io.InputStream;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Random;
+import java.util.Stack;
+
+public abstract class Ys {
+	public static String stringify(Object o) {
+		if(o == null) {
+			return "null";
+		} if(o instanceof Literal) {
+			return ((Literal)o).value.toString();
+		} else if(o instanceof Object[]) {
+			StringBuffer sb = new StringBuffer();
+			Object[] os = (Object[]) o;
+			sb.append("[");
+			for(int i = 0; i < os.length; i++) {
+				sb.append(" " + stringify(os[i]));
+			}
+			sb.append(" ]");
+			return sb.toString();
+		} else if(o instanceof Function) {
+			Function f = (Function) o;
+			return "Function( argc:" + f.argc
+				+ ", locals:" + f.locals.toString()
+				+ ", boxed:" + f.boxed.toString()
+				+ ", closure:" + f.closure.toString();
+		} else {
+			return o.toString();
+		}
+	}
+	private static class Literal {
+		public Object value;
+		public Literal(Object value) {
+			this.value = value;
+		}
+	}
+	private static class Function {
+		public int argc;
+		public Stack locals;
+		public Stack boxed;
+		public Stack closure;
+		public Function(Object[] list) {
+			Enumeration e;
+			argc = ((Object[])list[1]).length;
+			locals = new Stack();
+			boxed = new Stack();
+			closure = new Stack();
+
+			findIds(locals, list[1]);
+			findIds(locals, list[2]);
+
+			for(int i = 3; i < list.length; i++) {
+				findIds(closure, list[i]);
+			}
+
+			// local variables are not placed in the closure
+			e = locals.elements();
+			while(e.hasMoreElements()) {
+				closure.removeElement(e.nextElement());
+			}
+
+			// closure variables are automagically boxed
+			e = closure.elements();
+			while(e.hasMoreElements()) {
+				boxed.removeElement(e.nextElement());
+			}
+		}
+		private void findIds(Stack s, Object o) {
+			if(o instanceof Object[]) {
+				Object[] os = (Object[])o;
+				for(int i = 0; i < os.length; i++) {
+					findIds(s, os[i]);
+				}
+			} else if(o instanceof String) {
+				s.push(o);
+			} else if(o instanceof Function) {
+				Function f = (Function) o;
+				f.addOuter(s);
+				f.addOuter(boxed);
+			}
+		}
+		private void addOuter(Stack s) {
+			Enumeration e = closure.elements();
+			while(e.hasMoreElements()) {
+				Object o = e.nextElement();
+				if(!s.contains(o)) {
+					s.push(o);
+				}
+			}
+		}
+	}
+
+	private static class Builtin {
+		int opCode;
+		public Builtin(String name) {
+			opCode = builtins.indexOf(name);
+		}
+	}
+
+	private static Stack builtins;
+
+	static {
+		builtins = new Stack();
+		builtins.push("log");
+		builtins.push("set");
+	}
+
+	/////////////////////////////////////
+	// Factories
+	////
+	private static Object createId(String name) {
+		if(builtins.contains(name)) {
+			return new Builtin(name);
+		} else {
+			return name;
+		}
+	}
+	private static Object create(Object[] list) {
+		if(list.length > 0) {
+			Object fn = list[0];
+			if(fn.equals("function")) {
+				return new Function(list);
+			} 
+		}
+		return list;
+	}
+
+	/////////////////////////////////////
+	// The parser
+	////
+	private static final Object[] emptylist = new Object[0];
+	public static Object readExpression(InputStream is) throws IOException {
+		Stack stack = new Stack();
+		int c = is.read();
+		do {
+			// end of file
+			if (c == -1) {
+				return null;
+
+			// end of list
+			} else if (c == ']') {
+				Object result[];
+				c = is.read();
+				// find out how much of the stack
+				// is a part of the terminated list.
+				// null indicates a "["
+				int pos = stack.search(null);
+				// ] with no [ begun
+				if (pos == -1) {
+					result = emptylist;
+				} else {
+					// stack search includes the null, which we want to skip
+					pos--;
+					// move the elements from the stack
+					result = new Object[pos];
+					while (pos > 0) {
+						pos--;
+						result[pos] = stack.pop();
+					}
+					// pop the null
+					stack.pop();
+				}
+				// create the list obj
+				stack.push(create(result));
+
+
+			// Whitespace
+			} else if (c <= ' ') {
+				c = is.read();
+
+			// Comment
+			} else if (c == ';') {
+				do {
+					c = is.read();
+				} while (c > '\n');
+
+			// List
+			} else if (c == '[') {
+				// null is a marker of "["
+				stack.push(null);
+				c = is.read();
+
+			// Number
+			} else if ('0' <= c && c <= '9') {
+				int i = 0;
+				do {
+					i = i * 10 + c - '0';
+					c = is.read();
+				} while ('0' <= c && c <= '9');
+				stack.push(new Literal(new Integer(i)));
+
+			// String
+			} else if (c == '"') { // (comment ends '"' when prettyprinting)
+
+				StringBuffer sb = new StringBuffer();
+				c = is.read();
+				while (c != '"' && c != -1) { // (comment ends '"' when prettyprinting)
+
+					if (c == '\\') {
+						c = is.read();
+					}
+					sb.append((char) c);
+					c = is.read();
+				}
+				c = is.read();
+				stack.push(new Literal(sb.toString()));
+
+			// Identifier
+			} else {
+				StringBuffer sb = new StringBuffer();
+				while (c > ' ' && c != '[' && c != ']') {
+					sb.append((char) c);
+					c = is.read();
+				}
+				stack.push(createId(sb.toString()));
+			}
+		} while (stack.empty() || stack.size() > 1 || stack.elementAt(0) == null);
+		return stack.pop();
+	}
+}
