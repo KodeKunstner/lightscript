da1643fb08458c1fcb0ba4d6ddee20e15f8f2b42
voel <voel@cba0054c-e3dd-4c1e-9f5d-83818dded0c2>
2009-03-15 10:23:44 +0000

    git-svn-id: file://localhost/home/voel/image/svn/yolan@406 cba0054c-e3dd-4c1e-9f5d-83818dded0c2

--- code/Zui/src/Zui.java
+++ code/Zui/src/Zui.java
@@ -13,32 +13,18 @@ public class Zui extends Canvas implements CommandListener, Order {
 String names[] = {"a2ps", "AbiSuite-2.4", "aclocal", "acpi-support", "adduser", "aiksaurus",
 "alsa", "alsa-base", "ant", "apache2", "apmd", "app-install", "application-registry",
 "applications", "applnk", "apport", "apps", "apt", "aptitude", "apturl", "ardour2", "aspell",
-"audacity", "autostart", "avahi", "backgrounds", "base-files", "base-passwd", "bash", "binfmts", 
+/*"audacity", "autostart", "avahi", "backgrounds", "base-files", "base-passwd", "bash", "binfmts", 
 "binfmt-support", "bison", "brasero", "bug", "ca-certificates", "calendar", "classpath",
 "CMake", "cm-super", "command-not-found", "common-licenses", "config.kcfg", "consolefonts",
 "console-setup", "console-tools", "consoletrans", "cups", "cupsddk", "dbus-1", "debconf",
 "debhelper", "debianutils", "defoma", "desktop-directories", "dict", "dictionaries-common",
 "directfb-1.0.1", "displayconfig-gtk", "djvu", "doc", "doc-base", "dpkg", "e2fsprogs",
-"emacs", "emoticons", "enchant", "epdfview", "epiphany", "et", "evince", "faces", "file",
-"file-roller", "firefox", "fonts", "fuse-utils", "games", "gcalctool", "gcj", "gconf",
-"gdebi", "gdl", "gdm", "gettext", "gftp", "ggz", "ghostscript", "gimp", "gksu", "glchess",
-"glib-2.0", "gnect", "gnibbles", "gnobots2", "gnumeric", "gnupg", "goffice", "graphviz", "groff",
-"gst-python", "gstreamer-0.10", "gstreamer-properties", "gtk-doc", "gtk-engines", "gtkmathview",
-"gtksourceview-1.0", "guile", "gutenprint", "hal", "hosts", "hotkey-setup", "hplip", "i18n",
-"icons", "idl", "ifupdown", "im-switch", "info", "initramfs-tools", "initrd-tools", "initscripts",
-"inkscape", "installation-report", "isdn", "iso-codes", "java", "java-common", "javazi", "jockey",
-"keymaps", "keyrings", "ladspa", "language-selector", "language-support", "laptop-mode-tools",
-"launchpad-integration", "lcdf-typetools", "lftp", "libgda-3.0", "libgksu", "libgphoto2",
-"libthai", "lilypond", "linda", "link-grammar", "lintian", "linux-restricted-modules",
-"linux-sound-base", "locale", "locale-langpack", "locales", "lua", "man", "man-db", "menu",
-"metacity", "mime", "mime-info", "mimelnk", "misc", "mozilla", "mplayer", "myspell", "mysql",
-"mysql-common", "nano", "netbeans", "nm-applet", "nvidia-kernel-common", "ogonkify", "omf",
 "onboard", "openoffice", "openssl-blacklist", "orage", "ots", "pam", "perl", "perl5", "php5",
 "phpmyadmin", "pixmaps", "pkgconfig", "plt", "pmi", "pnm2ppa", "PolicyKit", "popularity-contest",
 "postgresql", "powernowd", "ppd", "ppp", "pycentral-data", "pygtk", "pyshared", "pyshared-data",
 "python", "python-apt", "python-support", "qt3", "qt4", "R", "readline", "recovery-mode",
 "sablevm", "sablevm-classpath", "samba", "sane", "scim", "screen", "scrollkeeper", "services",
-"servicetypes", "sgml", "sgml-base", "sgml-data", "snmp", "software-properties", "sounds", 
+"servicetypes", "sgml", "sgml-base", "sgml-data", "snmp", "software-properties", "sounds", */
 "squeak", "ssl-cert", "synaptic", "sysklogd", "system-config-printer", "system-tools-backends-2.0",
 "tabset", "tasksel", "tcltk", "templates", "Terminal", "terminfo", "tex4ht", "tex-common",
 "texinfo", "texlive-base", "texlive-bin", "texmf", "texmf-texlive", "themes", "thumbnailers",
@@ -58,8 +44,6 @@ String names[] = {"a2ps", "AbiSuite-2.4", "aclocal", "acpi-support", "adduser", 
         try {
 	    // Set up this canvas to listen to command events
 	    setCommandListener(this);
-	    // Add the Exit command
-	    addCommand(new Command("Exit", Command.EXIT, 1));
         } catch(Exception e) {
             e.printStackTrace();
         }
