cf86e2ef840217056aa57eac32e276e20bb30a59
voel <voel@cba0054c-e3dd-4c1e-9f5d-83818dded0c2>
2009-04-19 17:48:03 +0000

    bugfix
    git-svn-id: file://localhost/home/voel/image/svn/yolan@472 cba0054c-e3dd-4c1e-9f5d-83818dded0c2

--- code/LightScript/test.js
+++ code/LightScript/test.js
@@ -1,22 +1,5 @@
-print(1/3);
-x = {"foo": 1, "bar": 2};
-for(i in x) {
-        print(i + x[i]);
+function f(x) {
+    print(x * x);
 }
 
-a = {"foo": null, "bar": false, "baz": undefined};
-print(a);
-a = {"foo": 1, "bar": true, "baz": undefined};
-print(a);
-a = {"foo": 1, "bar": 2};
-print(a);
-
-for(x in a) {
-    print(x + a[x]);
-}
-
-j = 0;
-for(i=0;i<100;i++) {
-    j += i;
-}
-print(j);
+f(3);
