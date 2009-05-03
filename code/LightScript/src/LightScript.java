9d07558d8bcc48ca4557c1414bb666195b5bc86c
voel <voel@cba0054c-e3dd-4c1e-9f5d-83818dded0c2>
2009-05-03 17:01:12 +0000

    git-svn-id: file://localhost/home/voel/image/svn/yolan@551 cba0054c-e3dd-4c1e-9f5d-83818dded0c2

--- code/LightScript/src/LightScript.java
+++ code/LightScript/src/LightScript.java
@@ -2574,7 +2574,6 @@ public final class LightScript {
                 case ID_CALL_FN: {
                     int argc = code[++pc];
                     Object o = stack[sp - argc];
-                    thisPtr = stack[sp - argc - 1];
                     if(o instanceof Code) {
                         Code fn = (Code) o;
 
@@ -2600,8 +2599,7 @@ public final class LightScript {
                         try {
                             Object result = ((LightScriptFunction)o
                                 ).apply(thisPtr, stack, sp - argc + 1, argc);
-
-                            sp -= argc + 1 + RET_FRAME_SIZE;
+                            sp -= argc + RET_FRAME_SIZE;
                             stack[sp] = result;
                         } catch(LightScriptException e) {
                             if(exceptionHandler < 0) {
