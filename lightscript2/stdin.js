5d5421ca055da307cfb1863c6afaf14ac8bdc9e3
rasmusjensen <moc.buhtig@solsort.dk>
2009-09-28 21:00:11 +0200

    .

--- lightscript2/stdin.js
+++ lightscript2/stdin.js
@@ -1,19 +1,19 @@
-// Function that reads one character at a time
-getch = (function() {
+// stdin iterator
+stdin = { "__iterator__": function() { return this; },
+    "pos": -1,
+    "line": 0,
+    "next": getch = (function() {
     var line = "";
-    var line_pos = -1;
-    var line_nr = 0;
     var empty_line_count = 0;
     
     return function() {
-        line_pos = line_pos + 1;
-        line = line;
-        if(line[line_pos] !== undefined) {
+        this.pos = this.pos + 1;
+        if(line[this.pos] !== undefined) {
             empty_line_count = 0;
-            return line[line_pos];
+            return line[this.pos];
         } else {
-            line_nr = line_nr + 1;
-            line_pos = -1;
+            this.line = this.line + 1;
+            this.pos = -1;
             line = readline();
             empty_line_count = empty_line_count + 1;
             if(empty_line_count > 10) {
@@ -23,8 +23,4 @@ getch = (function() {
             } 
         }
     };
-})()
-
-while((c = getch()) !== undefined) {
-    print(c);
-}
+})()}
---
+++ lightscript2/tokeniser.js
@@ -0,0 +1,194 @@
+tokenise = function(inputstream) {
+    var buffer = "";
+    var line = 0;
+    var pos = 0;
+    var start;
+    var newline = false;
+    var comment = "";
+    var iterator = LightScriptIterator(inputstream);
+    
+    function len(list) {
+        return list.length;
+    }
+    
+    function within(elem, list) {
+        for(var e in LightScriptIterator(list)) {
+            if(e === elem) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    function one_of(str) {
+        return within(peek(), str);
+    }
+    
+    function starts_with(str) {
+        return peek(len(str)) === str;
+    }
+    
+    function ensure_buffer(size) {
+        while(len(buffer) < size) {
+            var c = inputstream.next();
+            if(c !== undefined) {
+                buffer = buffer + c;
+            } else {
+                buffer = buffer + "\0";
+            }
+        }
+    }
+    
+    function peek(n, pos) {
+        n = n || 1;
+        pos = pos || 0;
+        ensure_buffer(pos + n);
+        return buffer.slice(pos, pos+n);
+    }
+    
+    function pop(n) {
+        n = n || 1;
+        var result = peek(n);
+        if(result[0] == "\0") {
+            throw StopIteration;
+        }
+        buffer = buffer.slice(n);
+    
+        // keep track of position in stream
+        for(c in LightScriptIterator(result)) {
+            pos = pos + 1;
+            if(c === "\n") {
+                pos = 0;
+                line = line + 1;
+            }
+        }
+        return result;
+    }
+    
+    function current_position() {
+        return {"line": line, "pos": pos};
+    }
+    
+    function begin_token() {
+        start = current_position();
+    }
+    
+    function new_token(type, val) {
+        var result = {
+            "type": type, 
+            "val": val,
+            "start": start,
+            "end": current_position(),
+            "newline": newline,
+            "comment": comment };
+        newline = false;
+        comment = "";
+        return result;
+    }
+    
+    function next() {
+        var whitespace = " \t\r";
+        var single_symbol = "(){}[].:;,";
+        var joined_symbol = "=+-*/<>%!";
+        var ident = "_$qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+        var digits = "0123456789";
+        var hexdigits = digits + "abcdefABCDEF";
+        var s, c, quote;
+    
+        // repeat until token parsed
+        while(true) {
+    
+            // Keep track of postion
+            begin_token();
+    
+            // Whitespace
+            if(one_of(whitespace)) {
+                pop();
+    
+            // Comment
+            } else if(starts_with("//") || starts_with("#")) {
+                if(pop() === "/") {
+                    pop();
+                }
+                s = "";
+                while(peek() !== "\n") {
+                    s += pop();
+                }
+                comment = comment + s + "\n";
+                print("Comment: " + comment);
+    
+            // Unescaped string
+            } else if(starts_with('"""')) {
+                pop(3);
+                s = "";
+                while(!starts_with('"""')) {
+                    s += pop();
+                }
+                pop(3);
+                return new_token("string", s);
+    
+            // String
+            } else if(one_of("'\"")) {
+                s = "";
+                quote = pop();
+                while(!starts_with(quote)) {
+                    c = pop();
+                    if(c === "\\") {
+                        c = pop();
+                        c = {"n": "\n", "r": "\r", "t": "\t"}[c] || c;
+                    }
+                    s += c;
+                }
+    
+                // remove end-quote
+                pop();
+                return new_token("string", s);
+    
+            // Number
+            } else if(one_of(digits) || (one_of(".-") && within(peek(1,1), digits))) {
+                s = pop();
+    
+                // normal or hexadecimal
+                if(peek() !== 'x') {
+                    while(one_of(".-e" + digits)) {
+                        s += pop();
+                    }
+                } else {
+                    s = pop(2);
+                    while(one_of(hexdigit)) {
+                        s += pop();
+                    }
+                }
+                return new_token("number", s);
+    
+            // Symbol
+            } else if(one_of(single_symbol)) {
+                return new_token("symbol", pop());
+            } else if(one_of(joined_symbol)) {
+                s = "";
+                while(one_of(joined_symbol)) {
+                    s += pop();
+                }
+                return new_token("symbol", s);
+    
+            // Identifier
+            } else if(one_of(ident)) {
+                s = "";
+                while(one_of(ident + digits)) {
+                    s += pop();
+                }
+                return new_token("identifier", s);
+    
+            // Newline
+            } else if(peek() === "\n") {
+                pop();
+                newline = true;
+            } else if(peek() === "\0") {
+                throw StopIteration;
+            } else {
+                throw "Tokenisation error: " + peek().charCodeAt(0) + " '" + buffer + "' at line " + line;
+            }
+        }
+    }
+    return { "__iterator__": function() { return this; }, "next": next };
+}
