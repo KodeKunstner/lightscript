bcd95102be298a11f18ca9d69affdb139c30e854
rasmusjensen <moc.buhtig@solsort.dk>
2009-09-30 16:29:06 +0200

    refactoring

--- lightscript2/parser.js
+++ lightscript2/parser.js
@@ -15,111 +15,148 @@ function Token(type, val, subtype, start, end, newline, comment) {
     token.val = val;
     token.start = start;
     token.end = end;
-    token.newline = newline;
-    token.comment = comment;
+    if(comment != "") {
+        token.comment = comment;
+    }
+    if(newline) {
+        token.newline = newline;
+    }
+    if(subtype != undefined) {
+        token.subtype = subtype;
+    }
     return token;
 };
 
-default_nud = function() { 
-    this.children = [];
-    return this; 
-};
-
-syntax_error = function(str) {
-    return function() {
-        throw "Syntax error -  " + str + " at line " + this.start.line + "\nContent: " + this.val;
-    }
-};
-
-token_prototype = {};
+var token_prototype = {};
 token_prototype.bp = 0;
+token_prototype.led_op = function() { 
+    this.syntax_error(this.val + "' cannot be infixed");  
+}
+token_prototype.nud_op = function() { 
+    return this; 
+}
+token_prototype.comment = "";
+token_prototype.newline= false;
+token_prototype.children = [];
+
+// Utility functions
+token_prototype.syntax_error = function(msg) {
+    throw "Syntax error at line " + this.start.line + ": " + msg;
+}
+
 token_prototype.readlist = function(list) {
     if(this.rparen === undefined) {
-        throw "Error: no end-paren-type for '" + this.token_type + "' in line " + this.start.line;
+        throw "Internal error: no end-paren-type for '" + this.token_type + "'";
     }
-    while(token.val !== this.rparen) {
-
-        // TODO: handle end of file
-        list.push(parse());
-    }
+    try {
+        while(token.val !== this.rparen) {
+            // TODO: handle end of file
+            list.push(parse());
+        }
     token = next_token()
+    } catch(e) {
+        if(e === StopIteration) {
+            this.syntax_error("Unterminated list, missing " + this.rparen);
+        } else {
+            throw e;
+        }
+    }
     return list;
 }
 
-function led(name, op, bp, gen_id) {
-    if(bp !== undefined) {
-        tok(name).bp = bp;
+token_prototype.led = function(left) {
+    var obj = this;
+    if(obj.lid !== undefined) {
+        obj.id = obj.lid;
     }
-    if(typeof(gen_id) === "function") {
-        tok(name).led_op = op;
-        tok(name).lupdate = gen_id;
-        tok(name).led = function(left) {
-            var new_this = this.led_op(left);
-            return new_this.lupdate();
-        }
-    } else if(typeof(gen_id) === "string") {
-        tok(name).led_op = op;
-        tok(name).lid = gen_id;
-        tok(name).led = function(left) {
-            this.id = this.lid;
-            return this.led_op(left);
-        }
+    obj = obj.led_op(left);
+    if(typeof(obj.lupdate) === "function") {
+        return obj.lupdate();
     } else {
-        tok(name).led = op;
+        return obj;
     }
 }
 
-infix = function(left) {
+token_prototype.nud = function(left) {
+    var obj = this;
+    if(obj.nid !== undefined) {
+        obj.id = obj.nid;
+    }
+    obj = obj.nud_op();
+    if(typeof(obj.nupdate) === "function") {
+        return obj.nupdate();
+    } else {
+        return obj;
+    }
+}
+
+var l = function(name, op, bp, gen_id) {
+    tok(name).led_op = op;
+
+    if(bp !== undefined) {
+        tok(name).bp = bp;
+    }
+
+    if(typeof(gen_id) === "function") {
+        tok(name).lupdate = gen_id;
+    } 
+
+    if(typeof(gen_id) === "string") {
+        tok(name).lid = gen_id;
+    }
+
+}
+
+var n = function(name, op, gen_id) {
+    tok(name).nud_op = op;
+
+    if(typeof(gen_id) === "function") {
+        tok(name).nupdate = gen_id;
+    } 
+
+    if(typeof(gen_id) === "string") {
+        tok(name).nid = gen_id;
+    } 
+}
+
+var infix = function(left) {
         this.children = [left, parse(this.bp)]; 
         return this;
 }
 
-infixr = function(left) {
+var infixr = function(left) {
         this.children = [left, parse(this.bp - 1)]; 
         return this;
 }
 
-infixparen = function(left) {
+var infixparen = function(left) {
         this.children = this.readlist([left]);
         return this;
 }
 
-nud = function(name, op, gen_id) {
-    if(typeof(id) === "string") {
-        tok(name).nud_op = op;
-        tok(name).nid = gen_id;
-        tok(name).nud = function() {
-            this.id = this.nid;
-            return this.nud_op();
-        }
-    } else {
-        tok(name).nud = op;
-    }
-}
-
-prefix = function() {
+var prefix = function() {
     this.children = [parse()];
     return this;
 }
 
-prefix2 = function() {
+var prefix2 = function() {
     this.children = [parse(), parse()];
     return this;
 }
 
-atom = function() {
+var atom = function() {
     this.children = [];
     return this;
 }
 
-paren = function() {
+var paren = function() {
     this.children = this.readlist([]);
     return this;
 }
 
 
 var token_handler = {};
-function tok(id) {
+var tok = function(id) {
     if(token_handler[id] === undefined) {
         token_handler[id] = Object.create(token_prototype);
         token_handler[id].token_type = id;
@@ -128,77 +165,73 @@ function tok(id) {
 }
 
 // Default nuds/leds
-tok("(string)").nud = default_nud;
-tok("(string)").led = syntax_error("infix string");
-tok("(number)").nud = default_nud;
-tok("(number)").led = syntax_error("infix number");
-tok("(identifier)").nud = default_nud;
-tok("(identifier)").led = syntax_error("unsupported infix identifier");
-tok("(symbol)").nud = syntax_error("unsupported prefix symbol");
-tok("(symbol)").led = syntax_error("unsupported infix symbol");
+tok("(symbol)").nud_op = function() { this.syntax_error(this.val + " is not af prefix function"); }
 
 // Define what opposing parentheses matches
 tok("(").rparen = ")";
 tok("[").rparen = "]";
 tok("{").rparen = "}";
 
-// TODO: change variable name to string, and add flag.
-led(".", infix, 700, "subscript");
-
 // Leds
-led("(", infixparen, 600, "apply");
-led("[", infixparen, 600, "subscript");
-led("*", infix, 500, "mul");
-led("/", infix, 500, "div");
-led("%", infix, 500, "rem");
-led("+", infix, 400, "add");
-led("-", infix, 400, "sum");
-led("===", infix, 300, "eq");
-led("==", infix, 300, "eq");
-led("!=", infix, 300, "neq");
-led("!==", infix, 300, "neq");
-led("<=", infix, 300, "leq");
-led("<", infix, 300, "less");
-led(">=", infix, 300, "geq");
-led(">", infix, 300, "greater");
-led("&&", infixr, 200, "and");
-led("||", infixr, 200, "or");
-led("else", infixr, 200);
-led("=", infix, 100, "set");
-led("+=", infix, 100);
-led(",", infix, 50);
-led(":", infix, 50);
+l(".", infix, 700, "subscript", function() {
+        var key = this.children[1];
+        assert(key.type === "identifier");
+        key.type = "string";
+        key.subtype= "dot_identifier";
+});
+l("(", infixparen, 600, "apply");
+l("[", infixparen, 600, "subscript");
+l("*", infix, 500, "mul");
+l("/", infix, 500, "div");
+l("%", infix, 500, "rem");
+l("+", infix, 400, "add");
+l("-", infix, 400, "sum");
+l("===", infix, 300, "eq");
+l("==", infix, 300, "eq");
+l("!=", infix, 300, "neq");
+l("!==", infix, 300, "neq");
+l("<=", infix, 300, "leq");
+l("<", infix, 300, "less");
+l(">=", infix, 300, "geq");
+l(">", infix, 300, "greater");
+l("&&", infixr, 200, "and");
+l("||", infixr, 200, "or");
+l("else", infixr, 200);
+l("=", infix, 100, "set");
+l("+=", infix, 100);
+l(",", infix, 50);
+l(":", infix, 50);
 
 // Nuds
-nud("(", paren);
-nud("{", paren, "dict");
-nud("[", paren, "list");
-nud("var", prefix);
-nud("return", prefix, "return");
-nud("-", prefix, "neg");
-nud("!", prefix, "not");
-nud("function", prefix2, "function");
-nud("if", prefix2, "if");
-nud("while", prefix2, "while");
-nud(";", atom);
-nud("undefined", atom, "nil");
-nud("null", atom, "nil");
-nud("None", atom, "nil");
-nud("true", atom, "true");
-nud("false", atom, "false");
-nud("True", atom, "true");
-nud("False", atom, "false");
+n("(", paren);
+n("{", paren, "dict");
+n("[", paren, "list");
+n("var", prefix);
+n("return", prefix, "return");
+n("-", prefix, "neg");
+n("!", prefix, "not");
+n("function", prefix2, "function");
+n("if", prefix2, "if");
+n("while", prefix2, "while");
+n(";", atom);
+n("undefined", atom, "nil");
+n("null", atom, "nil");
+n("None", atom, "nil");
+n("true", atom, "true");
+n("false", atom, "false");
+n("True", atom, "true");
+n("False", atom, "false");
 
 // Test code, just run on standard in while in development
-tokens = tokenise(stdin);
+var tokens = tokenise(stdin);
 
-function next_token() {
+var next_token = function() {
         token = tokens.next();
         return token;
 }
 
 next_token();
-parse = function(rbp) {
+var parse = function(rbp) {
     rbp = rbp || 0;
     var t = token;
     next_token();
@@ -211,7 +244,7 @@ parse = function(rbp) {
     return left
 };
 
-parser = {
+var parser = {
     "__iterator__": function() { return this; },
     "next": parse
-}
+};
