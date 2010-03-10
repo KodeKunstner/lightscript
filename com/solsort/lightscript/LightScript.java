d977018d516f0e0c64af9635867e5ac8b6c36bc6
Rasmus Jensen <rje@panther.opasia.dk>
2010-03-10 07:49:14 +0100

    ...

--- com/solsort/lightscript/LightScript.java
+++ com/solsort/lightscript/LightScript.java
@@ -26,76 +26,8 @@ import java.util.Hashtable;
  */
 // </editor-fold>
 public final class LightScript {
-    // TODO: globals object
+    // <editor-fold desc="TRUE NULL FALSE UNDEFINED">
 
-    public static Object oldGlobalObject = null;
-    private static Hashtable globals;
-
-    /** Constructor, loading standard library */
-    public LightScript() {
-        globals = new Hashtable();
-        StdLib.register(this);
-    }
-
-    /** Shorthands for executing a LightScript function */
-    public static Object apply(Object thisPtr, Function f) throws ScriptException {
-        Object args[] = {thisPtr};
-        return f.apply(args, 0, 0);
-    }
-
-    /** Shorthands for executing a LightScript function */
-    public static Object apply(Object thisPtr, Function f, Object arg1) throws ScriptException {
-        Object args[] = {thisPtr, arg1};
-        return f.apply(args, 0, 1);
-    }
-
-    /** Shorthands for executing a LightScript function */
-    public static Object apply(Object thisPtr, Function f, Object arg1, Object arg2) throws ScriptException {
-        Object args[] = {thisPtr, arg1, arg2};
-        return f.apply(args, 0, 2);
-    }
-
-    /** Shorthands for executing a LightScript function */
-    public static Object apply(Object thisPtr, Function f, Object arg1, Object arg2, Object arg3) throws ScriptException {
-        Object args[] = {thisPtr, arg1, arg2, arg3};
-        return f.apply(args, 0, 3);
-    }
-
-    /** Shorthand for evaluating a string that contains LightScript code */
-    public Object eval(String s) throws ScriptException {
-        return eval(new ByteArrayInputStream(s.getBytes()));
-    }
-
-    /** Parse and execute LightScript code read from an input stream */
-    public Object eval(InputStream is) throws ScriptException {
-        Object result = UNDEFINED, t = UNDEFINED;
-        Compiler c = new Compiler(is, this);
-        Function stmt = c.compileNextStatement();
-        while(stmt != null) {
-            result = apply(oldGlobalObject, stmt);
-            stmt = c.compileNextStatement();
-        }
-        return result;
-    }
-
-    public Object[] getBox(Object key) {
-        Object box[] = (Object[]) globals.get(key);
-        if(box == null) {
-            box = new Object[1];
-            box[0] = UNDEFINED;
-            globals.put(key, box);
-        }
-        return box;
-    }
-    /** Set a global value for this execution context */
-    public void set(Object key, Object value) {
-        getBox(key)[0] = value;
-    }
-
-    /** Retrieve a global value from this execution context */
-    public Object get(Object key) {
-        return getBox(key)[0];
-    }
     /** The true truth value of results
      * of tests/comparisons within LightScript */
     public static final Object TRUE = new StringBuffer("true");
@@ -107,5 +39,83 @@ public final class LightScript {
      * of tests/comparisons within LightScript */
     public static final Object FALSE = new StringBuffer("false");
     // </editor-fold>
-    // <editor-fold desc="options">
+    //<editor-fold desc="globals">
+    /** Hashtable containing boxed global values */
+    private Hashtable globals = new Hashtable();
+
+    /** 
+     * Retrieve a boxed global.
+     * @param key the name of the global
+     * @return an array of length one, containing the global value.
+     */
+    public Object[] getBox(Object key) {
+        Object box[] = (Object[]) globals.get(key);
+        if (box == null) {
+            box = new Object[1];
+            box[0] = UNDEFINED;
+            globals.put(key, box);
+        }
+        return box;
+    }
+
+    /** Set a global value */
+    public void set(Object key, Object value) {
+        getBox(key)[0] = value;
+    }
+
+    /** Retrieve a global value */
+    public Object get(Object key) {
+        return getBox(key)[0];
+    }
+    // </editor-fold>
+
+    /** Constructor, loading standard library */
+    public LightScript() {
+        StdLib.register(this);
+    }
+
+    // <editor-fold desc="eval(...)">
+    /** Parse and execute LightScript code read from an input stream */
+    public Object eval(InputStream is) throws ScriptException {
+        Object result = UNDEFINED, t = UNDEFINED;
+        Compiler c = new Compiler(is, this);
+        Function stmt = c.compileNextStatement();
+        while (stmt != null) {
+            result = apply(this, stmt);
+            stmt = c.compileNextStatement();
+        }
+        return result;
+    }
+
+    /** Shorthand for evaluating a string that contains LightScript code */
+    public Object eval(String s) throws ScriptException {
+        return eval(new ByteArrayInputStream(s.getBytes()));
+    }
+    // </editor-fold>
+    //<editor-fold desc="apply(...)">
+
+    /** Shorthands for executing a LightScript function */
+    public Object apply(Object thisPtr, Function f) throws ScriptException {
+        Object args[] = {thisPtr};
+        return f.apply(args, 0, 0);
+    }
+
+    /** Shorthands for executing a LightScript function */
+    public Object apply(Object thisPtr, Function f, Object arg1) throws ScriptException {
+        Object args[] = {thisPtr, arg1};
+        return f.apply(args, 0, 1);
+    }
+
+    /** Shorthands for executing a LightScript function */
+    public Object apply(Object thisPtr, Function f, Object arg1, Object arg2) throws ScriptException {
+        Object args[] = {thisPtr, arg1, arg2};
+        return f.apply(args, 0, 2);
+    }
+
+    /** Shorthands for executing a LightScript function */
+    public Object apply(Object thisPtr, Function f, Object arg1, Object arg2, Object arg3) throws ScriptException {
+        Object args[] = {thisPtr, arg1, arg2, arg3};
+        return f.apply(args, 0, 3);
+    }
+    // </editor-fold>
 }
