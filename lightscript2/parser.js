1279403c023aac091b2e7da67a7ca690e1c9db27
Rasmus Jensen <rje@panther.opasia.dk>
2010-11-20 16:40:54 +0100

    ...

--- lightscript2/parser.js
+++ lightscript2/parser.js
@@ -1,13 +1,7 @@
 ////////////////////////////////////////////
 // Utility functions
 //
-clone = function(obj) {
-    var F = function() { };
-    F.prototype = obj;
-    return new F();
-}
-
-getch = (function() {
+var getch = (function() {
     var line = '';
     var pos;
     var newl = 0;
@@ -29,32 +23,10 @@ getch = (function() {
     }
 })();
 
-var isArray = function(o) {
-    return o.constructor === Array;
-};
+same = function(a, b) {
+    return uneval(a) === uneval(b);
+}
 
-var deepcopy = function(o) {
-    var i, result;
-    if (typeof(o) === 'string') {
-        return o;
-    } else if(isArray(o)) {
-        result = [];
-        i = 0;
-        while (i < o.length) {
-            result.push(deepcopy(o[i]));
-            i = i + 1;
-        }
-        return result;
-    } else {
-        result = {};
-        for(i in o) {
-            result[i] = deepcopy(o[i]);
-        }
-        return result
-    }
-};
-
-var identity = function(x) { return x; };
 
 /////////////////////////////////////////////
 // Tokeniser
@@ -79,6 +51,9 @@ var pop_string = function() {
 var symb = '=!<>&|/*+-%';
 var num = '1234567890';
 var alphanum = num + '_qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
+var separator = ';,:';
+
+var EOF = ["(eof)"];
 
 var next_token = function() {
     while (char_is(' \n\r\t')) {
@@ -99,19 +74,21 @@ var next_token = function() {
             push_char();
         }
         skip_char();
-        return fetch_token('string', pop_string());
+        return ['string', pop_string()];
     } else if (char_is(num)) {
         while (char_is(num)) {
             push_char();
         }
-        return fetch_token('number', pop_string());
+        return ['number', pop_string()];
+
+    // varname
     } else if (char_is(alphanum)) {
         while (char_is(alphanum)) {
             push_char();
         }
-        return fetch_token(pop_string());
+        return [pop_string()];
 
-        // read comment or multi character symbol
+    // read comment or multi character symbol
     } else if (char_is(symb)) {
         if (c === '/') {
             push_char();
@@ -121,94 +98,57 @@ var next_token = function() {
                 while (c !== undefined && c !== '\n') {
                     push_char();
                 }
-                return fetch_token('comment', pop_string());
+                return ['comment', pop_string()];
             }
         }
         while (char_is(symb)) {
             push_char();
         }
-        return fetch_token(pop_string());
+        return [pop_string()];
+    } else if (char_is(separator)) {
+        push_char();
+        return ['(sep)', pop_string()];
     } else if (c === undefined) {
-        return fetch_token('(eof)');
+        return EOF;
     } else  {
         push_char();
-        return fetch_token(pop_string());
+        return [pop_string()];
     }
-};
-
-////////////////////////////////////////////
-// Token creation
-//
-var fetch_token = function(type, val) {
-    var obj = token_types[type];
-    if (obj === undefined || obj.nud === undefined) {
-        val = type;
-        obj = token_types['identifier'];
-    }
-
-    if (val !== undefined) {
-        obj = clone(obj);
-        obj.val = val;
-    }
-    return obj;
-};
-
-super_token = {
-'nud': function() {
-        return ['parse-error', 'undefined-nud', this.id, this.val];
-    },
-'bp':
-    0,
-'led':
-    function(left) {
-        return ['parse-error', 'undefined-led', this.id, this.val, left];
-    }
-};
-
-token_types = {};
-
-var tok = function(name) {
-    var result = token_types[name];
-    if (result === undefined) {
-        result = clone(super_token);
-        result.id = name;
-        token_types[name] = result;
-    }
-    return result;
 };
 
 ///////////////////////////////////////
 // Operator constructors
 //
-fnMacro = function(expr) {
-    expr.child.unshift({name: "identifier", val: expr.name, child: []});
-    expr.name = 'call';
-}
 
-var infix = function(id, bp, fn) {
-    tok(id).bp = bp;
-    tok(id).led = function(left) {
-    return {name: this.id, 'child': [left, parse(this.bp)] }; };
-    macros[id] = fn || fnMacro;
+bp = {};
+led = {};
+nud = {};
+
+var infix = function(id, prio) {
+    bp[id] = prio;
+    led[id] = function(left, token) { 
+        return [id, left, parse(prio)];
+    };
 };
 
-var infixr = function(id, bp) {
-    tok(id).bp = bp;
-    tok(id).led = function(left) { return {name: this.id, 'child': [left, parse(this.bp - 1)] }; };
+var infixr = function(id, prio) {
+    bp[id] = prio;
+    led[id] = function(left, token) { 
+        return [id, left, parse(prio - 1)];
+    };
 };
 
-var infixlist = function(id, endsymb, bp, macro) {
-    var name = 'apply' + id;
-    tok(id).bp = bp;
-    tok(id).endsymb = endsymb;
-    tok(id).led = function(left) { return {"name": name, 'child': readlist([left], this.endsymb) }; };
-    macros[name] = macro;
+var infixlist = function(id, endsymb, prio) {
+    bp[id] = prio;
+    led[id] = function(left, token) { 
+        return readlist(["apply" + id, left], endsymb);
+    };
 };
 
 var readlist = function(acc, endsymb) {
-    while (token.id !== endsymb && token.id !== '(eof)') {
+    while (!same(token, endsymb) && token !== EOF) {
         var t = parse();
-        if (!t.isSeparator) {
+        if(t[0] !== "(sep)") {
             acc.push(t);
         }
     }
@@ -216,81 +156,40 @@ var readlist = function(acc, endsymb) {
     return acc;
 };
 
-var list = function(id, endsymb, name) {
-    tok(id).endsymb = endsymb;
-    tok(id).nud = function() { return {name: name, 'child': readlist([], this.endsymb)}; }
+var list = function(id, endsymb) {
+    nud[id] = function() { return readlist(["list" + id], endsymb); };
 };
 
-atom = function(id) {
-    tok(id).nud = function() { return {name: "identifier", val: this.id, 'child': [] }; }
+var passthrough = function(id) {
+    nud[id] = function(token) { return token; };
 };
 
-sep = function(id) {
-    tok(id).sep = true;
-    tok(id).nud = function() { return {name: '(separator)' , isSeparator: true, 'child': [id] }; }
+
+var prefix = function(id) {
+    nud[id] = function() { return [id, parse()]; };
 };
 
-prefix = function(id) {
-    tok(id).nud = function() { return {name: this.id, 'child': [parse()] }; }
-};
-
-prefix2 = function(id, macro) {
-    macro = macro || identity;
-    tok(id).nud = function() { return {name: this.id, 'child': [parse(), parse()]}; }
-    macros[id] = macro;
-};
-
-literal = function(id) {
-    tok(id).nud = function() { 
-        return {
-            name: this.id, 
-            'val': this.val, 
-            'child': []}; 
-    };
+var prefix2 = function(id) {
+    nud[id] = function() { return [id, parse(), parse()]}; 
 };
 
 //
 // Parser
 //
-macros = { default: identity};
-
-applymacros = function(expr) {
-    (macros[expr.name] 
-        || macros.default)(expr);
-    return expr;
-}
+var default_nud= function(o) { o.unshift("id"); return o;};
 
 parse = function(rbp) {
     rbp = rbp || 0;
     var t = token;
     token = next_token();
-    var left = applymacros(t.nud());
-    while (rbp < token.bp 
-            && !t.sep) {
+    var left = (nud[t[0]] || default_nud)(t);
+    while (rbp < (bp[token[0]] || 0)
+            && t[0] !== "(sep)") {
         t = token;
         token = next_token();
-        left = applymacros(t.led(left));
+        left = led[t[0]](left, t);
     };
     return left;
-};
-
-
-// return a function, that if the first expression argument is a subscript
-// changes the expression to a function call, and otherwise changes the expression type
-// to a given name.
-var subscriptFunctionOrName = function(fn, name) {
-    return function(expr) {
-        if (expr.child[0].name == 'call' 
-                && expr.child[0].child[0].name == 'identifier'
-                && expr.child[0].child[0].val == '[]') {
-            expr.child.unshift(expr.child[0].child[1]);
-            expr.child[1] = expr.child[1].child[2];
-            expr.name = fn;
-            fnMacro(expr);
-        } else {
-            expr.name = name;
-        };
-    };
 };
 
 //
@@ -298,19 +197,9 @@ var subscriptFunctionOrName = function(fn, name) {
 //
 // Definition of operator precedence and type
 //
-infix('.', 700, function(expr) {
-    expr.child[1].name = 'string';
-    expr.name = '[]';
-    fnMacro(expr);
-});
-
-infixlist('(', ')', 600, subscriptFunctionOrName(".()", "call"));
-
-infixlist('[', ']', 600, function(expr) { 
-    expr.name = '[]'; 
-    fnMacro(expr); 
-}); 
-
+infix('.', 700);
+infixlist('(', [')'], 600);
+infixlist('[', [']'], 600);
 infix('*', 500); 
 infix('%', 500);
 infix('/', 500);
@@ -322,81 +211,69 @@ infix('!==', 300);
 infix('!=', 300);
 infix('<=', 300); 
 infix('<', 300);
-
 infix('>=', 300);
 infix('>', 300);
 infixr('&&', 200); 
 infixr('||', 200); 
-
 infix('=', 100);
-
 infix('in', 50);
-
-sep(':');
-sep(';');
-sep(',');
-list('(', ')', 'paren');
-list('{', '}', 'object');
-macros['object'] = function(expr) {
-    var i = 0;
-    while(i < expr.child.length) {
-        if(expr.child[i].name === 'identifier') {
-            expr.child[i].name = 'string';
-        }
-        i = i + 2;
-    };
-};
-list('[', ']', 'array');
-list('var', ';', 'locals'); 
+list('(', [')']);
+list('{', ['}']);
+list('[', [']']);
+list('var', ['(sep)', ';']);
 prefix('return'); 
 prefix('-'); 
 prefix('!'); 
-macros['!'] = fnMacro;
 prefix('throw'); 
-macros['throw'] = fnMacro;
-atom('undefined'); 
-atom('null'); 
-atom(']');
-atom(')');
-atom('}');
-atom('(eof)');
-tok('null').id = 'undefined';
-atom('true'); 
-atom('false'); 
-literal('identifier');
-literal('string');
-literal('number');
-literal('comment');
+passthrough('undefined'); 
+passthrough('null'); 
+passthrough('(sep)');
+passthrough(')');
+passthrough('}');
+passthrough('(eof)');
+passthrough('true'); 
+passthrough('false'); 
+passthrough('identifier');
+passthrough('string');
+passthrough('number');
+passthrough('comment');
 infixr('else', 200);
-
-prefix2('while', function(node) {
-    // assume node.child[0] === { name:'paren', child:[?]}
-    node.child[0] = node.child[0].child[0];
-    // assume node.child[1] is curlybraces
-    node.child[1].name = 'block';
-}); 
-prefix2('for', function(node) {
-    // assume node.child[0] === { name:'paren', child:[{name:'in', child:[?, ?]}]}
-    node.child.unshift(node.child[0].child[0].child[0]);
-    node.child[0] = node.child[1].child[0].child[0];
-    node.child[1] = node.child[1].child[0].child[1];
-    node.child[2].name = 'block';
-    node.name = "for-in"
-}); 
+prefix2('while'); 
+prefix2('for'); 
 prefix2('if');
-prefix2('function'); // TODO
-prefix2('try'); // TODO
-prefix2('catch'); //TODO
+prefix2('function');
+prefix2('try');
+prefix2('catch');
 
 //////////////////////////////////////////
 // Build parse tree
-token = next_token();
 
 //
 // dump
 //
-t = readlist(['list{'], '');
-for (elem in t) {
-    print('');
-    print(uneval(t[elem]));
+
+var isArray = function(o) {
+        return o.constructor === Array;
+};
+
+var list2str = function(o) {
+    if(isArray(o)) {
+        var i = 0;
+        var result = [];
+        while(i < o.length) {
+            result.push(list2str(o[i]));
+           i = i + 1;
+        }
+        return "[" + result.join(", ") + "]";
+    } else {
+        return uneval(o);
+    }
+};
+//while((token = next_token())[0] != '(eof)') {
+//   print(token);
+//}
+
+token = next_token();
+while((x = parse()) !== EOF) {
+    print(list2str(x));
 }
